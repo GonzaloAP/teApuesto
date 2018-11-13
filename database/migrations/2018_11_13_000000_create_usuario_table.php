3877a012c775399d3ee8bc645be43535cc2e30a6
Gonzalo <gonza.uap@gmail.com>
2018-11-13 09:00:45 -0400

    primer commit

---
+++ database/migrations/2018_11_13_000000_create_usuario_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsuarioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('cod')->unique();
+            $table->string('ci')->unique();
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('correo')->unique();
+            $table->timestamp('correo_verificado')->nullable();
+            $table->string('alias');
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuario');
+    }
+}
